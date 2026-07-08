94119d5f537cec553d72792e95c2a190790fc934
CHIRAZ GHODHBEN <chr277158@gmail.com>
2026-07-08 13:24:34 +0100

    fix: إضافة تعريفات bcryptjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,16 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        sans: ['Comic Sans MS', 'Tahoma', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
